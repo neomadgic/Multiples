81eb8aaddbce22feb6426d5cb3895f752f4fd880
Vu Dang <jublusion5@gmail.com>
2015-11-26 02:13:09 -0600

    connected graphics with code. Added function to play Button

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    //Variables
+    var currentNumber = 0
+    var multiplesNum = 0
+    var maxNum = 5
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var inputBar: UITextField!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var messageLbl: UILabel!
+    
+    @IBAction func whenPlayBtnPushed(sender: UIButton)
+    {
+        if(inputBar.text != nil && inputBar.text != "")
+        {
+            multiplesNum = Int(inputBar.text!)!
+            logo.hidden = true;
+            playBtn.hidden = true;
+            inputBar.hidden = true;
+            addBtn.hidden = false;
+            messageLbl.text = "Press add to add \(multiplesNum) to 0";
+            messageLbl.hidden = false;
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func whenAddBtnPushed(sender: UIButton)
+    {
     }
 
 
